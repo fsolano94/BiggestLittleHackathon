ee89ac2dacd7cd7f8618de82d8b1f92eb289d549
Franky Solano <fsolano@nevada.unr.edu>
2017-09-30 15:19:35 -0700

    Updated Solution Files

---
+++ Application/Application/About.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="Application.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2 id="H2" runat="server">My name.</h2>
+    <h3>Your application description page.</h3>
+    <p>
+        <asp:TextBox ID="UserInput" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="OnButtonClicked" />
+    </p>
+    <p id="URLDisplayMessage" runat="server"></p>
+    <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+</asp:Content>
---
+++ Application/Application/About.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Ajax.Utilities;
+using System.Xml;
+
+namespace Application
+{
+    public partial class About : Page
+    {
+        private WebRequest _webRequest;
+        private WebResponse _webResponse;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public static bool IsValidURL(string strURL)
+        {
+            Uri uriResult;
+            bool result = Uri.TryCreate(strURL, UriKind.Absolute, out uriResult)
+                          && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            return result;
+        }
+
+        public void OnButtonClicked(object sender, EventArgs parameters)
+        {
+            if (IsValidURL(UserInput.Text) == false)
+            {
+                URLDisplayMessage.InnerHtml = "ERROR:Invalid URL ";
+            }
+            else
+            {
+                URLDisplayMessage.InnerHtml = "Now going to the web page ...";
+                Uri urlAddress = new Uri(UserInput.Text);
+                _webRequest = WebRequest.Create(urlAddress);
+            }
+            GotoWebPage();
+        }
+
+        public void GotoWebPage(object sender = null, EventArgs parameters=null)
+        {
+           
+        }
+
+    }
+}
